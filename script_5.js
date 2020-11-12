871495109af394fa0c82c6a0ed0ed2d1098db763
dediane <dianedecourt@gmail.com>
2020-11-12 19:59:01 +0100

    script 5 finished

--- script_5.js
+++ script_5.js
@@ -13,6 +13,14 @@ const books = [
     { title: 'Guerre et Paix', id: 748147, rented: 19 }
   ];
 
+  function display(array)
+{
+    for(var i = 0; i < array.length; i++)
+    {
+        console.log(array[i]);
+    }
+}
+
   //Est-ce que tous les livres ont été au moins empruntés une fois ?
   console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?")
   let flag = 0;
@@ -25,3 +33,44 @@ const books = [
   {
       console.log("Tous les livres ont été loués.")
   }
+
+  //Quel est livre le plus emprunté ?
+  console.log("\nQuel est livre le plus emprunté ?\n")
+  books.sort(function(a, b){
+    if(a.rented < b.rented) {return 1;}
+    if(a.rented > b.rented) {return -1;}
+    return 0;
+  })
+  console.log('Le livre le plus emprunté est ' + books[0].title + " emprunté " + books[0].rented + " fois");
+
+  //Quel est le livre le moins emprunté ?
+  console.log("\nQuel est le livre le moins emprunté ?\n")
+  books.sort(function(a, b){
+      if(a.rented < b.rented) {return -1;}
+      if(a.rented > b.rented) {return 1;}
+      return 0;
+  })
+  console.log('Le livre le moins emprunté est ' + books[0].title + " emprunté " + books[0].rented + " fois");
+
+  //Trouve le livre avec l'ID: 873495
+  console.log("\nTrouve le livre avec l'ID: 873495\n");
+  console.log(books.find(book => book.id === 873495));
+
+  //Supprime le livre avec l'ID: 133712
+  console.log("\nSupprime le livre avec l'ID: 133712\n");
+  console.log("Suppression de :")
+  console.log(books.find(book => book.id == 133712));
+for (let i in books) {
+  if (books[i].id === 133712) {
+    books.splice(i, 1);
+  }
+}
+
+//Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé)
+console.log("\nTrie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé)\n\n");
+books.sort(function (a , b) 
+{
+    return a.title.localeCompare(b.title)
+})
+  display(books);
+
