344aca4652ae5752e50eb1d5320b254ae96818f8
dediane <dianedecourt@gmail.com>
2020-11-12 18:47:16 +0100

    script 4 finished

---
+++ script_5.js
@@ -0,0 +1,27 @@
+const books = [
+    { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
+    { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
+    { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
+    { title: 'Les frères Karamazov', id: 450911, rented: 55 },
+    { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
+    { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
+    { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
+    { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
+    { title: 'La disparition', id: 364445, rented: 33 },
+    { title: 'La lune seule le sait', id: 63541, rented: 43 },
+    { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
+    { title: 'Guerre et Paix', id: 748147, rented: 19 }
+  ];
+
+  //Est-ce que tous les livres ont été au moins empruntés une fois ?
+  console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?")
+  let flag = 0;
+  books.forEach(book => book.rented === 0 && flag++);
+  if(flag > 0)
+  {
+    console.log("Tous les livres n'ont pas été loués.")
+  }
+  else
+  {
+      console.log("Tous les livres ont été loués.")
+  }
